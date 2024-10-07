4da0bff33dd12a113df1251b3d886f7832e689d7
Trikest <sal.00000850311@unicap.br>
2024-10-07 20:18:30 -0300

    fist commit

---
+++ src/main/java/com/example/gestao_projetos/entity/Mebros.java
@@ -0,0 +1,51 @@
+package com.example.gestao_projetos.entity;
+
+
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Mebros {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    private String funcao;
+    public Mebros() {  }
+
+    public Mebros(String nome, String funcao) {
+        this.nome = nome;
+        this.funcao = funcao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Mebros{" +
+               "id=" + id +
+               ", nome='" + nome + '\'' +
+               ", salario=" + funcao +
+               '}';
+    }
+    
+}
